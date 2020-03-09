d45aed0d23e59047b25fd19cf4a439f86fa02dde
zhusonger <449350247@qq.com>
2020-03-09 10:50:48 +0800

    提交初始化脚本

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "开始git clone任务"
+echo "开始Base项目"
+rm -rf base
+mkdir base
+git clone git@github.com:zhusonger/androidz_base.git base
+echo "完成Base项目"
+echo "开始Widget项目"
+rm -rf widget
+mkdir widget
+git clone git@github.com:zhusonger/androidz_widget.git widget
+echo "完成Widget项目"
+echo "开始Media项目"
+rm -rf media
+mkdir media
+git clone git@github.com:zhusonger/androidz_media.git media
+echo "完成Media项目"
+echo "完成git clone任务"
+echo "添加jecenter参数"
+echo bintrayUser="zhusong" >> local.properties
+echo groupId="cn.com.lasong" >> local.properties
+echo bintrayKey="XXXFromApi" >> local.properties
+echo "添加完成"
