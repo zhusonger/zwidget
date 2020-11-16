c426058e75a34ce74147f01208622a54cbb00271
zhusong <song.zhu@lasong.com.cn>
2020-11-16 20:25:01 +0800

    更新widget库 添加顶部弹窗

--- app/src/main/java/cn/com/lasong/widget/WidgetActivity.java
+++ app/src/main/java/cn/com/lasong/widget/WidgetActivity.java
@@ -1,11 +1,13 @@
 package cn.com.lasong.widget;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import cn.com.lasong.R;
 import cn.com.lasong.base.BaseActivity;
+import cn.com.lasong.widget.dialog.TopSheetDialog;
 
 /**
  * Author: zhusong
@@ -19,5 +21,23 @@ public class WidgetActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widget);
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_top_sheet: {
+                TopSheetDialog dialog = new TopSheetDialog(this);
+                dialog.setContentView(R.layout.view_lyric);
+                // 是否拦截触摸事件
+                dialog.setConsumeTouch(false);
+                // 在控件外触摸不隐藏
+                dialog.setCanceledOnTouchOutside(false);
+                // 可隐藏
+                dialog.setCancelable(true);
+                dialog.show();
+                break;
+            }
+        }
     }
 }
