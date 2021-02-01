95f90155b7aba9a562e753bf81305d5e5ef6a790
zhusong <song.zhu@lasong.com.cn>
2021-02-01 18:49:40 +0800

    添加主题 兼容刘海屏

--- app/src/main/java/cn/com/lasong/widget/WidgetActivity.java
+++ app/src/main/java/cn/com/lasong/widget/WidgetActivity.java
@@ -1,13 +1,20 @@
 package cn.com.lasong.widget;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
+import cn.com.lasong.MainActivity;
 import cn.com.lasong.R;
 import cn.com.lasong.base.BaseActivity;
 import cn.com.lasong.widget.dialog.TopSheetDialog;
+import cn.com.lasong.widget.utils.ViewHelper;
 
 /**
  * Author: zhusong
@@ -21,21 +28,30 @@ public class WidgetActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widget);
+        ViewHelper.transparentStatusBar(this);
     }
 
+    TopSheetDialog dialog;
     public void onClick(View view) {
         int id = view.getId();
         switch (id) {
             case R.id.btn_top_sheet: {
-                TopSheetDialog dialog = new TopSheetDialog(this);
+                if (null != dialog)
+                    dialog.dismiss();
+                dialog = new TopSheetDialog(this);
                 dialog.setContentView(R.layout.view_lyric);
+                // 可隐藏
+                dialog.setCancelable(true);
                 // 是否拦截触摸事件
                 dialog.setConsumeTouch(false);
                 // 在控件外触摸不隐藏
                 dialog.setCanceledOnTouchOutside(false);
-                // 可隐藏
-                dialog.setCancelable(true);
                 dialog.show();
+                break;
+            }
+            case R.id.btn_new_page: {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
                 break;
             }
         }
