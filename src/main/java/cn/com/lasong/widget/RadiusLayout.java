191258751c481b9b6c5a813cf1037652dc0231fe
zhusong <song.zhu@lasong.com.cn>
2021-05-11 13:38:26 +0800

    圆角背景布局添加边线

--- src/main/java/cn/com/lasong/widget/RadiusLayout.java
+++ src/main/java/cn/com/lasong/widget/RadiusLayout.java
@@ -27,23 +27,30 @@ public class RadiusLayout extends RelativeLayout {
 
     final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Path bgPath = new Path();
+    Path borderPath = new Path();
     // 标记边界需要圆角的值
     int borderFlags;
     int bgColor;
     float bgRadius;
+    int borderWidth;
+    int borderColor;
     public RadiusLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.RadiusLayout);
         bgColor = ta.getColor(R.styleable.RadiusLayout_bgColor, Color.TRANSPARENT);
         bgRadius = ta.getDimension(R.styleable.RadiusLayout_bgRadius, Color.TRANSPARENT);
-        borderFlags = ta.getInt(R.styleable.ShadowLayout_radiusFlags, ViewHelper.BORDER_ALL);
+        borderFlags = ta.getInt(R.styleable.RadiusLayout_radiusFlags, ViewHelper.BORDER_ALL);
+        borderWidth = ta.getDimensionPixelSize(R.styleable.RadiusLayout_borderWidth, 0);
+        borderColor = ta.getColor(R.styleable.RadiusLayout_borderColor, Color.TRANSPARENT);
         ta.recycle();
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
+
+        int saveCount = canvas.save();
+
         if (bgColor != Color.TRANSPARENT) {
-            int saveCount = canvas.save();
             final float left = getPaddingLeft();
             final float top = getPaddingTop();
             final float right = getWidth() - getPaddingRight();
@@ -51,10 +58,26 @@ public class RadiusLayout extends RelativeLayout {
             bgPath = ViewHelper.updateRadiusPath(bgPath, left, top, right, bottom,
                     bgRadius, bgRadius, borderFlags);
 
+            mPaint.setStyle(Paint.Style.FILL);
             mPaint.setColor(bgColor);
             canvas.drawPath(bgPath, mPaint);
-            canvas.restoreToCount(saveCount);
         }
+
+        if (borderWidth > 0 && borderColor != Color.TRANSPARENT) {
+            final float offset = borderWidth * 1.0f / 2;
+            final float left = getPaddingLeft() + offset;
+            final float top = getPaddingTop() + offset;
+            final float right = getWidth() - getPaddingRight() - offset;
+            final float bottom = getHeight() - getPaddingBottom() - offset;
+            borderPath = ViewHelper.updateRadiusPath(borderPath, left, top, right, bottom,
+                    bgRadius - offset, bgRadius - offset, borderFlags);
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeWidth(borderWidth);
+            mPaint.setColor(borderColor);
+            canvas.drawPath(borderPath, mPaint);
+        }
+
+        canvas.restoreToCount(saveCount);
         super.dispatchDraw(canvas);
     }
 }
