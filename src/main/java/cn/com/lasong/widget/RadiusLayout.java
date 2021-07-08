5f393dfe6cf6721bc9b6816c9f0e20235c5bc2e5
zhusong <song.zhu@lasong.com.cn>
2021-07-08 15:10:03 +0800

    完成设置页

--- src/main/java/cn/com/lasong/widget/RadiusLayout.java
+++ src/main/java/cn/com/lasong/widget/RadiusLayout.java
@@ -15,6 +15,7 @@ import cn.com.lasong.widget.utils.ViewHelper;
  * Email: song.zhu@lasong.com.cn
  * Date: 2021/4/29
  * Description:
+ * 圆角背景
  */
 public class RadiusLayout extends RelativeLayout {
     public RadiusLayout(Context context) {
@@ -28,6 +29,7 @@ public class RadiusLayout extends RelativeLayout {
     final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Path bgPath = new Path();
     Path borderPath = new Path();
+    boolean bgCircle;
     // 标记边界需要圆角的值
     int borderFlags;
     int bgColor;
@@ -37,8 +39,9 @@ public class RadiusLayout extends RelativeLayout {
     public RadiusLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.RadiusLayout);
+        bgCircle = ta.getBoolean(R.styleable.RadiusLayout_bgCircle, false);
         bgColor = ta.getColor(R.styleable.RadiusLayout_bgColor, Color.TRANSPARENT);
-        bgRadius = ta.getDimension(R.styleable.RadiusLayout_bgRadius, Color.TRANSPARENT);
+        bgRadius = ta.getDimension(R.styleable.RadiusLayout_bgRadius, 0);
         borderFlags = ta.getInt(R.styleable.RadiusLayout_radiusFlags, ViewHelper.BORDER_ALL);
         borderWidth = ta.getDimensionPixelSize(R.styleable.RadiusLayout_borderWidth, 0);
         borderColor = ta.getColor(R.styleable.RadiusLayout_borderColor, Color.TRANSPARENT);
@@ -46,34 +49,56 @@ public class RadiusLayout extends RelativeLayout {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        final int left = getPaddingLeft();
+        final int top = getPaddingTop();
+        final int right = w - getPaddingRight();
+        final int bottom = h - getPaddingBottom();
+
+        bgPath = ViewHelper.updateRadiusPath(bgPath, left, top, right, bottom,
+                bgRadius, bgRadius, borderFlags);
+
+        final float offset = borderWidth * 1.0f / 2;
+        final float borderLeft = left + offset;
+        final float borderTop = top + offset;
+        final float borderRight = right - offset;
+        final float borderBottom = bottom - offset;
+        borderPath = ViewHelper.updateRadiusPath(borderPath, borderLeft, borderTop, borderRight, borderBottom,
+                bgRadius - offset, bgRadius - offset, borderFlags);
+    }
+
+    @Override
     protected void dispatchDraw(Canvas canvas) {
 
         int saveCount = canvas.save();
 
+        final float left = getPaddingLeft();
+        final float top = getPaddingTop();
+        final float right = getWidth() - getPaddingRight();
+        final float bottom = getHeight() - getPaddingBottom();
+        final float cx = (right - left) / 2;
+        final float cy = (bottom - top) / 2;
+        final float radius = Math.min(cx, cy);
         if (bgColor != Color.TRANSPARENT) {
-            final float left = getPaddingLeft();
-            final float top = getPaddingTop();
-            final float right = getWidth() - getPaddingRight();
-            final float bottom = getHeight() - getPaddingBottom();
-            bgPath = ViewHelper.updateRadiusPath(bgPath, left, top, right, bottom,
-                    bgRadius, bgRadius, borderFlags);
-
             mPaint.setStyle(Paint.Style.FILL);
             mPaint.setColor(bgColor);
-            canvas.drawPath(bgPath, mPaint);
+            if (bgCircle) {
+                canvas.drawCircle(cx, cy, radius, mPaint);
+            } else {
+                canvas.drawPath(bgPath, mPaint);
+            }
         }
 
         if (borderWidth > 0 && borderColor != Color.TRANSPARENT) {
-            final float offset = borderWidth * 1.0f / 2;
-            final float left = getPaddingLeft() + offset;
-            final float top = getPaddingTop() + offset;
-            final float right = getWidth() - getPaddingRight() - offset;
-            final float bottom = getHeight() - getPaddingBottom() - offset;
-            borderPath = ViewHelper.updateRadiusPath(borderPath, left, top, right, bottom,
-                    bgRadius - offset, bgRadius - offset, borderFlags);
             mPaint.setStyle(Paint.Style.STROKE);
             mPaint.setStrokeWidth(borderWidth);
             mPaint.setColor(borderColor);
+            if (bgCircle) {
+                canvas.drawCircle(cx, cy, radius - borderWidth * 1.0f / 2, mPaint);
+            } else {
+                canvas.drawPath(bgPath, mPaint);
+            }
             canvas.drawPath(borderPath, mPaint);
         }
 
